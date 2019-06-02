f933608dac884b2590318551b2e96a729befb571
magicpieh28 <choi.jung_won.ca1@is.naist.jp>
2019-06-02 20:14:00 +0900

    classifier and softmax

--- kadai2/classifier.py
+++ kadai2/classifier.py
@@ -1,22 +1,35 @@
 import numpy as np
-import random
 
 
-def softmax(z: np.ndarray):
-	return np.exp(z) / np.sum(np.exp(z))
+class SoftmaxRegression(object):
+	def __init__(self, lr: float, l2: float, batch_num: int, class_num: int):
+		self.lr = lr
+		self.l2 = l2
+		self.batch_num = batch_num
+		self.class_num = class_num
 
+		self.W_ = np.random.random((self.batch_num, self.class_num))
+		self.b_ = np.array([0.01, 0.1, 0.1])
 
-class Classifier(object):
-	def __init__(self, X: np.ndarray):
-		self.W = np.random.random((X.shape[1]))
-		self.b = np.array(0.)
+	def predict_prob(self, X):
+		z = self.net(X)
+		prob = self.softmax(z)
+		return prob
 
-	def forward(self, X: np.ndarray):
-		z = np.dot(X, self.w) + self.b
-		return softmax(z)
+	def net(self, X: np.ndarray):
+		return self.softmax(np.dot(X, self.W_) + self.b_)
 
-	def backward(self, X: np.ndarray, output: np.ndarray, target: np.ndarray):
-		n = X.shape[0]
-		grad = -np.dot(X, target - output) / n
-		bias = np.mean(target - output)
-		return grad, bias
+	@staticmethod
+	def softmax(z: np.ndarray):
+		return (np.exp(z.T) / np.sum(np.exp(z), axis=1)).T
+
+	@staticmethod
+	def find_argmax(z: np.ndarray):
+		return z.argmax(axis=1)
+
+	@staticmethod
+	def cross_entropy(output: np.ndarray, target: np.ndarray):
+		return - np.sum(target * np.log(output), axis=1)
+
+	def calculate_cost(self, output: np.ndarray, target: np.ndarray):
+		return np.mean(self.cross_entropy(output, target))
